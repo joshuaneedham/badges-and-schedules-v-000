f137e07ec426cedd406808f75412375d27bd5344
Joshua Needham <joshuabneedham@gmail.com>
2017-10-23 22:02:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,16 @@
 # Write your code here.
-def badge_maker
-  
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
 end
 
 def batch_badge_creator
-  
+
 end
 
 def assign_rooms
-  
+
 end
 
 def printer
-  
+
 end
