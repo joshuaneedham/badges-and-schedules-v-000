711269be7911ef79c6483e400315ad6c36f9be1d
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 14:56:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map { |name, index| "Hello, #{name}! You'll be assigned to room #{index}!"   } 
+  attendees.each_with_index.map { |name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"   } 
 end
 
 def printer
